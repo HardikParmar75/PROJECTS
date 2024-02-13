6819c4d628c0439de9cccdfdb94e22885d406133
HardikParmar75 <hardikparmar7557@gmail.com>
2024-02-13 15:44:42 +0530

    .

---
+++ portfolio/vesper-bootstrap-js/js/main.js
@@ -0,0 +1,8 @@
+// var apps = document.getElementsByClassName('a');
+// var cards = document.getElementsByClassName('c');
+// var webs = document.getElementsByClassName('w');
+// // console.log(apps)
+// // console.log(cards)
+// // console.log(webs)
+// var img1 = document.getElementById('img1s')
+// console.log(img1)
